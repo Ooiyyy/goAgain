47fdbc1dd5f4d7643f5fd871447125e758c383fd
Ooiyyy <oiypes59@gmail.com>
2026-03-31 15:06:11 +0700

    edit user with authorization

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module goAgain
 
 go 1.26.1
 
-require github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.9.3 // indirect
+	github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
+	golang.org/x/crypto v0.49.0 // indirect
+)
--- jwt.go
+++ jwt.go
@@ -1,23 +1,33 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"strings"
 	"time"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type LoginReq struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
+type UpdateUserReq struct {
+	Password string `json:"password"`
+}
 
 func main() {
+	connectDB()
 	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/register", registerHandler)
 	http.HandleFunc("/profile", jwtMiddleware(profileHandler))
+	http.HandleFunc("/update", jwtMiddleware(updateUserHandler))
 	fmt.Println("Server jalan di :8080")
 	http.ListenAndServe(":8080", nil)
 }
@@ -46,6 +56,23 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	var username string
+	var password string
+
+	query := "SELECT username, password FROM users where username = ?"
+	err = db.QueryRow(query, req.Username).Scan(&username, &password)
+	if err != nil {
+		jsonError(w, http.StatusUnauthorized, "User tidak ditemukan")
+	}
+	// if req.Password != password {
+	// 	jsonError(w, http.StatusUnauthorized, "Password salah")
+	// }
+	err = bcrypt.CompareHashAndPassword([]byte(password), []byte(req.Password))
+	if err != nil {
+		jsonError(w, http.StatusUnauthorized, "password salah")
+		return
+	}
+
 	if req.Username == "" {
 		// w.WriteHeader(http.StatusBadRequest)
 		// w.Write([]"Username wajib diisi") (bisa pakai cara ini atau yang atas)
@@ -58,12 +85,12 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 		jsonError(w, http.StatusBadRequest, "Password wajib diisi")
 		return
 	}
-	if req.Username != "admin" || req.Password != "1234" {
-		// w.WriteHeader(http.StatusUnauthorized)
-		// w.Write([]byte("login gagal"))
-		jsonError(w, http.StatusUnauthorized, "Username atau password salah")
-		return
-	}
+	// if req.Username != "admin" || req.Password != "1234" {
+	// 	// w.WriteHeader(http.StatusUnauthorized)
+	// 	// w.Write([]byte("login gagal"))
+	// 	jsonError(w, http.StatusUnauthorized, "Username atau password salah")
+	// 	return
+	// }
 
 	claims := jwt.MapClaims{
 		"username": req.Username,
@@ -93,12 +120,124 @@ func jwtMiddleware(next http.HandlerFunc) http.HandlerFunc {
 			jsonError(w, http.StatusUnauthorized, "Token tidak valid")
 			return
 		}
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if !ok {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		username, ok := claims["username"].(string)
+		if !ok {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), "username", username)
+		r = r.WithContext(ctx)
 		next(w, r)
 	}
 }
 
 func profileHandler(w http.ResponseWriter, r *http.Request) {
+	username := r.Context().Value("username").(string)
 	json.NewEncoder(w).Encode(map[string]string{
-		"message": "ini data rahasia",
+		"message":  "ini data rahasia",
+		"username": username,
+	})
+}
+
+var db *sql.DB
+
+func connectDB() {
+	dsn := "root:Gridy20@tcp(127.0.0.1:3306)/testdb"
+
+	database, err := sql.Open("mysql", dsn)
+	if err != nil {
+		panic(err)
+	}
+	err = database.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	db = database
+}
+
+func registerHandler(w http.ResponseWriter, r *http.Request) {
+	var req LoginReq
+	if r.Method != "POST" {
+		jsonError(w, http.StatusMethodNotAllowed, "Method harus POST")
+		return
+	}
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		jsonError(w, http.StatusBadRequest, "Invalid JSON")
+		return
+	}
+	if req.Username == "" {
+		jsonError(w, http.StatusBadRequest, "Username wajib diisi")
+		return
+	}
+	if req.Password == "" {
+		jsonError(w, http.StatusBadRequest, "Password wajib diisi")
+		return
+	}
+	if len(req.Password) < 6 {
+		jsonError(w, http.StatusBadRequest, "password minimal 6 karakter")
+		return
+	}
+	var existing string
+	err = db.QueryRow("SELECT username FROM users WHERE username = ?", req.Username).Scan(&existing)
+
+	if err == nil {
+		jsonError(w, http.StatusBadRequest, "username sudah digunakan")
+		return
+	}
+
+	hashedPass, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+	if err != nil {
+		jsonError(w, http.StatusInternalServerError, "gagal hash Password")
+	}
+
+	query := "INSERT INTO users (username, password) VALUES (?, ?)"
+	_, err = db.Exec(query, req.Username, string(hashedPass))
+	if err != nil {
+		jsonError(w, http.StatusInternalServerError, "gagal insert user")
+		return
+	}
+	json.NewEncoder(w).Encode(map[string]string{
+		"message": "user berhasil dibuat",
+	})
+}
+
+func updateUserHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "PUT" {
+		jsonError(w, http.StatusMethodNotAllowed, "Method harus PUT")
+		return
+	}
+	username := r.Context().Value("username").(string)
+
+	var req UpdateUserReq
+	err := json.NewDecoder(r.Body).Decode((&req))
+	if err != nil {
+		jsonError(w, http.StatusBadRequest, "Invalid JSON")
+		return
+	}
+
+	if req.Password == "" {
+		jsonError(w, http.StatusBadRequest, "password wajib diisi")
+	}
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+	if err != nil {
+		jsonError(w, http.StatusInternalServerError, "gagal hash password")
+		return
+	}
+	query := "UPDATE users SET password = ? WHERE username = ?"
+	_, err = db.Exec(query, string(hashedPassword), username)
+	if err != nil {
+		jsonError(w, http.StatusInternalServerError, "gagal update user")
+		return
+	}
+	json.NewEncoder(w).Encode(map[string]string{
+		"message": "password berhasil diupdate",
 	})
 }
